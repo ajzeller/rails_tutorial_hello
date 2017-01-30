6d3188c72b10d6371da593efebd180ba46cf8827
Andrew <andrew.j.zeller@gmail.com>
2017-01-30 10:32:11 -0600

    fixed routes file for hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#helloRb'
+  root 'application#hello'
   # For details on the DSL available within this file, s
   # see http://guides.rubyonrails.org/routing.html
 end
